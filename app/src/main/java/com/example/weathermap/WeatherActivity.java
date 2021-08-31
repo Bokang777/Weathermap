ec047a3bc0b9e211c95220ee38da252ce2777c73
Bokang Ntshihlele <U15317324@tuks.co.za>
2021-08-31 13:33:12 +0200

    WeatherMap
    Features
    -Map View
    -Buildings View
    -Maps API
    -OpenWeatherMap API
    
    Version 5 with 2nd activity Pager

--- app/src/main/java/com/example/weathermap/WeatherActivity.java
+++ app/src/main/java/com/example/weathermap/WeatherActivity.java
@@ -2,13 +2,120 @@ package com.example.weathermap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.weathermap.model.Location;
+import com.example.weathermap.model.Weather;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class WeatherActivity extends AppCompatActivity {
+
+    String city = "London, UK";
+
+    private TextView cityText;
+    private TextView condDescr;
+    private TextView temp;
+    private TextView press;
+    private TextView windSpeed;
+    private TextView windDeg;
+
+    private TextView hum;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        TextView cityText = (TextView) findViewById(R.id.cityText);
+        TextView condDescr = (TextView) findViewById(R.id.condDescr);
+        TextView temp = (TextView) findViewById(R.id.temp);
+        TextView hum = (TextView) findViewById(R.id.hum);
+        TextView press = (TextView) findViewById(R.id.press);
+        TextView windSpeed = (TextView) findViewById(R.id.windSpeed);
+        TextView windDeg = (TextView) findViewById(R.id.windDeg);
+        ImageView imgView = (ImageView) findViewById(R.id.condIcon);
+
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{city});
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = jsonWeatherParse.getWeather(data);
+
+                // Let's retrieve the icon
+                weather.iconData = ( (new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if (weather.iconData != null && weather.iconData.length > 0) {
+                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+                imgView.setImageBitmap(img);
+            }
+
+            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
+            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+            temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "�C");
+            hum.setText("" + weather.currentCondition.getHumidity() + "%");
+            press.setText("" + weather.currentCondition.getPressure() + " hPa");
+            windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+            windDeg.setText("" + weather.wind.getDeg() + "�");
+
+        }
+
+
+
+
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/weathermap/parseJson.java
@@ -0,0 +1,4 @@
+package com.example.weathermap;
+
+public class parseJson {
+}
